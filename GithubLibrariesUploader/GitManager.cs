5b76690a0ba73357084b9f204e624df8b9ceece3
Yotic <yoticcitoy@gmail.com>
2025-03-02 15:16:17 +0300

    Added git manager.

---
+++ GithubLibrariesUploader/GitManager.cs
@@ -0,0 +1,37 @@
+﻿using Korn.Utils;
+
+class GitManager : IDisposable
+{
+    public GitManager(string projectPath)
+    {
+        cmd = new CommandLine();
+        cmd.WriteLine($"cd \"{projectPath}\"");
+    }
+
+    CommandLine cmd;
+
+    GitManager Execute(string line)
+    {
+        cmd.WriteLine(line);
+        return this;
+    }
+
+    public GitManager Pull() => Execute("git pull");
+    public GitManager Push() => Execute("git push");
+    public GitManager Add(string path) => Execute($"git add {path}");
+    public GitManager Commit(string message) => Execute($"git commit -m \"{message}\"");
+
+    #region IDisposable
+    bool disposed;
+    public void Dispose()
+    {
+        if (disposed)
+            return;
+        disposed = true;
+
+        cmd.Dispose();
+    }
+
+    ~GitManager() => Dispose();
+    #endregion
+}
--- GithubLibrariesUploader/Program.cs
+++ GithubLibrariesUploader/Program.cs
@@ -1,49 +1,52 @@
 ﻿using Korn;
-using Korn.Utils;
 
 var kornPath = @"C:\Data\programming\vs projects\korn";
 var overviewProjectPath = Path.Combine(kornPath, "Overview");
-var librariesPath = Path.Combine(overviewProjectPath, "Binaries", "Libraries");
+var binariesPath = Path.Combine(overviewProjectPath, "Binaries");
+var librariesPath = Path.Combine(binariesPath, "Libraries");
 
-var cmd = new CommandLine();
-cmd.WriteLine($"cd \"{overviewProjectPath}\"");
-cmd.WriteLine($"git pull");
+using var git = new GitManager(overviewProjectPath);
+git.Pull();
 
 foreach (var file in Directory.GetFiles(librariesPath, "*.*", SearchOption.AllDirectories))
     if (Path.GetFileName(file) != "plug")
         File.Delete(file);
 
-var libraries = Korn.Interface.ServiceModule.Libraries.DefaultLibraries;
-foreach (var library in libraries)
+UploadLibraries();
+
+git
+.Add(@"Binaries")
+.Push()
+.Pull();
+
+void UploadLibraries()
 {
-    var projectName = library;
-    var libraryProjectPath = Path.Combine(kornPath, projectName, projectName);
-    if (!Directory.Exists(libraryProjectPath))
-        throw new KornException(
-            $"Unable find a project with name {projectName}."
-        );
-
-    var binariesPath = Path.Combine(libraryProjectPath, "bin", "x64", "Debug");
-    var versionsDirectories = Directory.GetDirectories(binariesPath);
-
-    var net8Directory = versionsDirectories.FirstOrDefault(directory => Path.GetFileName(directory) == "net8.0-windows");
-    var net472Directory = versionsDirectories.FirstOrDefault(directory => Path.GetFileName(directory) == "net472");
-
-    Move(net8Directory, Path.Combine(librariesPath, "net8"));
-    Move(net472Directory, Path.Combine(librariesPath, "net472"));
-
-    void Move(string? fromFolder, string toFolder)
+    var libraries = Korn.Interface.ServiceModule.Libraries.DefaultLibraries;
+    foreach (var library in libraries)
     {
-        if (fromFolder is null)
-            return;
+        var projectName = library;
+        var libraryProjectPath = Path.Combine(kornPath, projectName, projectName);
+        if (!Directory.Exists(libraryProjectPath))
+            throw new KornException(
+                $"Unable find a project with name {projectName}."
+            );
 
-        foreach (var fileName in (string[])[projectName + ".dll", projectName + ".pdb"])
-            File.Move(Path.Combine(fromFolder, fileName), Path.Combine(toFolder, fileName));
+        var binariesPath = Path.Combine(libraryProjectPath, "bin", "x64", "Debug");
+        var versionsDirectories = Directory.GetDirectories(binariesPath);
+
+        var net8Directory = versionsDirectories.FirstOrDefault(directory => Path.GetFileName(directory) == "net8.0-windows");
+        var net472Directory = versionsDirectories.FirstOrDefault(directory => Path.GetFileName(directory) == "net472");
+
+        Move(net8Directory, Path.Combine(librariesPath, "net8"));
+        Move(net472Directory, Path.Combine(librariesPath, "net472"));
+
+        void Move(string? fromFolder, string toFolder)
+        {
+            if (fromFolder is null)
+                return;
+
+            foreach (var fileName in (string[])[projectName + ".dll", projectName + ".pdb"])
+                File.Copy(Path.Combine(fromFolder, fileName), Path.Combine(toFolder, fileName));
+        }
     }
 }
-
-cmd.WriteLine($"git add Binaries\\Libraries\\net8");
-cmd.WriteLine($"git add Binaries\\Libraries\\net472");
-cmd.WriteLine($"git commit -m \"auto: uploaded libraries\"");
-cmd.WriteLine($"git push");
-cmd.WriteLine($"git pull");
